5d8a34f817e493f29ace0df203b87e639d35d65e
Danel Morales <danelmoraless@hotmail.com>
2026-07-04 17:09:49 -0500

    estilos

---
+++ src/estilos/index.estilo.ts
@@ -0,0 +1,17 @@
+//pagina de diseno para index
+import { StyleSheet } from 'react-native';
+
+export const estilos = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    backgroundColor: '#E6F1FB',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 24,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#0F6E56',
+  },
+});
